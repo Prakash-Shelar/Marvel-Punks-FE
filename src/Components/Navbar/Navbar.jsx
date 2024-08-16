ff515fcf1ca5bf32c8fb8b46c586ca69eaca3034
Palakpreet Singh <shibsshib011@gmail.com>
2024-08-16 23:15:56 +0530

    Server started

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
   return (
     <nav>
-      <motion.img 
+      <img 
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
